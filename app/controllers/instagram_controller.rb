d48e59ee93a642ece954c1f796abfaff6179a675
hsy505 <xiaoyaocompany@gmail.com>
2016-02-07 17:37:27 +0800

    2016-2-7

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,7 +1,7 @@
 class InstagramController < ApplicationController
   def index
     require 'net/http'
-    res = Net::HTTP.get(URI('https://api.instagram.com/v1/users/self/media/recent?access_token=23782837.a423953.00520dd971f94949b0394a859c96fa59&count=20'))
+    res = Net::HTTP.get(URI('https://api.instagram.com/v1/users/self/media/recent?access_token=23782837.a423953.244d7c44ebe245a7a794a1031a191493&count=20'))
     body = JSON.parse(res)
     @images = body['data'].map{|d| d['images']['standard_resolution']['url']}
   end
